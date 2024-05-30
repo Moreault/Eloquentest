5c985837b9dc6895d902e3bcac3f0713946d4956
Mathieu Moreault <seranx@gmail.com>
2024-05-30 15:20:25 -0400

    Updated Dummies version

--- Eloquentest.Dummies.Tests/DummyEnumerableCustomization.cs
+++ Eloquentest.Dummies.Tests/DummyEnumerableCustomization.cs
@@ -3,14 +3,14 @@
     [AutoCustomization]
     public sealed class ProperEnumerableCustomization : ListCustomizationBase
     {
-        public override IEnumerable<Type> Types { get; } = [typeof(Garbage.ProperEnumerable<>)];
+        protected override IEnumerable<Type> Types { get; } = [typeof(Garbage.ProperEnumerable<>)];
         protected override object Convert<T>(IEnumerable<T> source) => new Garbage.ProperEnumerable<T>(source);
     }
 
     [AutoCustomization]
     public sealed class BadEnumerableCustomization : ListCustomizationBase
     {
-        public override IEnumerable<Type> Types { get; } = [typeof(Garbage.BadEnumerable<>)];
+        protected override IEnumerable<Type> Types { get; } = [typeof(Garbage.BadEnumerable<>)];
         protected override object Convert<T>(IEnumerable<T> source) => new Garbage.BadEnumerable<T>(source);
     }
 }
