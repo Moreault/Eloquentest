a96609ee9cdcdf9b6ccf684e4e39bbef843588c3
Mathieu Moreault <seranx@gmail.com>
2024-09-27 15:56:45 -0400

    AssemblyInitializer no longer required

--- Eloquentest.Dummies.Tests/AssemblyInitializer.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace Eloquentest.Dummies.Tests;
-
-////TODO 3.0.0 Figure out a way to load the assembly automatically
-//[TestClass]
-//public static class AssemblyInitializer
-//{
-//    [AssemblyInitialize]
-//    public static void AssemblyInitialize(TestContext _) => AssemblyInitializerBase.Initialize("Eloquentest.Dummies");
-//}
